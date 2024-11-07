3b37047828456deac1ee88883ef5c999d0fbdda5
KJH198 <kjh15071687155@126.com>
2024-11-08 01:07:10 +0800

    登录界面作为第一界面

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,3 @@
 from backEnd import DB_Initiator, login
-import subprocess
 DB_Initiator.init()
 login.run()
-# subprocess.run(['C:\\Users\\kjh15\\AppData\\Roaming\\npm\\npm.cmd', 'run', 'build'], check=True,cwd ="Code\\frontEnd")
